779df4c6398a4c7bfeff62281de75831d3abbba4
kevokagwima <kevokagwima@gmail.com>
2025-10-07 16:21:38 +0300

    Updated files

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,13 +1,28 @@
-export default function MovieCard() {
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
+
+const MovieCard: React.FC<MovieProps> = ({
+  title,
+  posterImage,
+  releaseYear,
+}) => {
   return (
-    <div>
-      <h1>Title:</h1>
-      <p>Year of release</p>
-      <p>Actors:</p>
-      <ul>
-        <li>John Cena</li>
-        <li>Jackie Chan</li>
-      </ul>
+    <div className="h-[563px]">
+      <div>
+        <Image
+          className="h-[430px] w-full rounded-md hover:cursor-pointer"
+          src={posterImage}
+          width={100}
+          height={100}
+          alt={title}
+        />
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
     </div>
   );
-}
+};
+
+export default MovieCard;
